c2b5d1d5bd992bb9ceafd271353e03f169ffb4d2
Leon <leon.mainusch@web.de>
2023-11-21 10:58:17 +0100

    FIX: handle multiple TT

--- TerminTabelle.js
+++ TerminTabelle.js
@@ -1,9 +1,24 @@
-function receiveMessage(event) {
-    // Handle the message received from the iframe
-    console.log('Message from iframe:', event.data)
-    if (String(event.data).startsWith('tt-height')) {
-      const height = event.data.split('=')[1]
-      document.getElementById('tt-iframe').style.height = `${height}px`
+function receiveMessage (event) {
+  // Handle the message received from the iframe
+  if (String(event.data).startsWith('tt-height')) {
+    // Find the iframe from which the message is sent
+    let sourceFrame = null
+    const frames = document.getElementsByTagName('iframe')
+    for (let i = 0; i < frames.length; i++) {
+      if (frames[i].contentWindow === event.source) {
+        sourceFrame = frames[i]
+        break
+      }
     }
+
+    if (!sourceFrame) {
+      console.error('Could not find iframe')
+      return
+    }
+
+    // Set the height of the iframe
+    const height = event.data.split('=')[1]
+    sourceFrame.style.height = `${height}px`
   }
-  window.addEventListener('message', receiveMessage, false)
+}
+window.addEventListener('message', receiveMessage, false)
