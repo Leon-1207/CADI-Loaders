7a63c0bfbe50a7ea9abbb80157571b365d102928
Leon <leon.mainusch@web.de>
2024-07-02 10:43:27 +0200

    FIX: Safari private mode

--- TerminTabelle.js
+++ TerminTabelle.js
@@ -1,24 +1,52 @@
+const knownDateTableIFrames = []
+
+function getSourceFrame (event) {
+  const isEventIFrame = iFrame => iFrame.contentWindow === event.source
+
+  // search in known iframes
+  for (let i = 0; i < knownDateTableIFrames.length; i++) {
+    if (isEventIFrame(knownDateTableIFrames[i])) {
+      return knownDateTableIFrames[i]
+    }
+  }
+
+  // search in all iframes
+  const frames = document.getElementsByTagName('iframe')
+  for (let i = 0; i < frames.length; i++) {
+    if (isEventIFrame(frames[i])) {
+      knownDateTableIFrames.push(frames[i])
+      return frames[i]
+    }
+  }
+}
+
+function setIframeHeight (iFrame, msg) {
+  const height = msg.split('=')[1]
+  iFrame.style.height = `${height}px`
+}
+
+function ttRedirect (iFrame, msg) {
+  const urlParts = msg.split('=')
+  urlParts.shift() // Remove the first element
+  const url = urlParts.join('=') // Join the remaining parts
+  console.log('Redirecting (target blank) to:', url)
+  window.open(url, '_blank')
+}
+
 function receiveMessage (event) {
   // Handle the message received from the iframe
-  if (String(event.data).startsWith('tt-height')) {
-    // Find the iframe from which the message is sent
-    let sourceFrame = null
-    const frames = document.getElementsByTagName('iframe')
-    for (let i = 0; i < frames.length; i++) {
-      if (frames[i].contentWindow === event.source) {
-        sourceFrame = frames[i]
-        break
-      }
-    }
+  const msg = String(event.data)
+  let fn = null
+  if (msg.startsWith('tt-height')) fn = setIframeHeight
+  else if (msg.startsWith('tt-redirect')) fn = ttRedirect
 
-    if (!sourceFrame) {
-      console.error('Could not find iframe')
-      return
-    }
-
-    // Set the height of the iframe
-    const height = event.data.split('=')[1]
-    sourceFrame.style.height = `${height}px`
+  // Find the iframe from which the message is sent
+  const sourceFrame = getSourceFrame(event)
+  if (!sourceFrame) {
+    console.error('Could not find iframe')
+    return
   }
+
+  fn(sourceFrame, msg) // Call the appropriate function
 }
 window.addEventListener('message', receiveMessage, false)
