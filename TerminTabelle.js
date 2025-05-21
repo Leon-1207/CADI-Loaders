7dadd51ff8f32513e836f1e01cc49af28a08ac4c
Leon <leon.mainusch@web.de>
2025-05-21 11:35:55 +0200

    Update TerminTabelle.js

--- TerminTabelle.js
+++ TerminTabelle.js
@@ -1,26 +1,37 @@
-console.log('TerminTabelle.js started loading')
-function receiveMessage (event) {
-  // Handle the message received from the iframe
-  if (String(event.data).startsWith('tt-height')) {
-    // Find the iframe from which the message is sent
-    let sourceFrame = null
-    const frames = document.getElementsByTagName('iframe')
-    for (let i = 0; i < frames.length; i++) {
-      if (frames[i].contentWindow === event.source) {
-        sourceFrame = frames[i]
-        break
-      }
-    }
+;(function () {
+  console.log('TerminTabelle.js started loading')
 
-    if (!sourceFrame) {
-      console.error('Could not find iframe')
+  // Handle the message received from the iframe
+  function receiveMessage (event) {
+    const msg = String(event.data)
+
+    if (msg.startsWith('tt-height')) {
+      const sourceFrame = getSourceFrame(event)
+      if (!sourceFrame) {
+        console.error('Could not find iframe')
+        return
+      }
+      // Set the height of the iframe
+      const height = event.data.split('=')[1]
+      sourceFrame.style.height = `${height}px`
       return
     }
 
-    // Set the height of the iframe
-    const height = event.data.split('=')[1]
-    sourceFrame.style.height = `${height}px`
+    if (msg.startsWith('redirect')) {
+      const url = msg.replace('redirect:', '')
+      window.location.href = url
+      return
+    }
   }
-}
-window.addEventListener('message', receiveMessage, false)
-console.log('TerminTabelle.js finished loading')
+
+  function getSourceFrame (event) {
+    const frames = document.getElementsByTagName('iframe')
+    for (let i = 0; i < frames.length; i++) {
+      if (frames[i].contentWindow === event.source) return frames[i]
+    }
+    return null
+  }
+
+  window.addEventListener('message', receiveMessage, false)
+  console.log('TerminTabelle.js finished loading')
+})()
